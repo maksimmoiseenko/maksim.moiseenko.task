d1a71789de2e722bfa1e0cacf38d1c981bd2a038
maksim.moiseenko <maksim.moiseenko.1999@yandex.ru>
2021-03-22 01:44:41 +0300

    deleted unnecessary code in interceptor

--- TestFrontend/testFrontend/src/app/TokenInterceptor.ts
+++ TestFrontend/testFrontend/src/app/TokenInterceptor.ts
@@ -10,14 +10,7 @@ export class TokenInterceptor implements HttpInterceptor {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    if (req.url.indexOf('login') !== -1 ) {
-      return next.handle(req);
-    }
-
     const token = localStorage.getItem('token');
-    console.log(token);
-
     if (token) {
       const reqWithAuth = req.clone({
         setHeaders: {
