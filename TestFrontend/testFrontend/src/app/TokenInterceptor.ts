2659bb41fc9b3d9280dcf61118e599cc8b3e36f9
maksim.moiseenko <maksim.moiseenko.1999@yandex.ru>
2021-03-24 15:20:10 +0300

    checking url v2

--- TestFrontend/testFrontend/src/app/TokenInterceptor.ts
+++ TestFrontend/testFrontend/src/app/TokenInterceptor.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +9,11 @@ import {Observable} from 'rxjs';
 export class TokenInterceptor implements HttpInterceptor {
   constructor() {
   }
+  baseUrl = environment.apiUrl;
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    if (req.url.includes('http://localhost:8080')){
+    if (req.url.includes(this.baseUrl)){
       const token = localStorage.getItem('token');
       console.log(token);
       if (token) {
